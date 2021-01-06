a1eb0b37d765117f324e60971570926aa8d6e490
Manhattan <manhattansgate@gmail.com>
2021-01-07 00:04:09 +0500

    a

---
+++ src/main/java/com/example/CVmaker/CVmakerApplication.java
@@ -0,0 +1,13 @@
+package com.example.CVmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CVmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CVmakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CVmaker/CVmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CVmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CVmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
